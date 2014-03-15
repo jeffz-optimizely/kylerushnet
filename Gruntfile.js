3c61648bd1f82a636b756dd771dd113963a24ddd
Kyle Rush <kyle@kylerush.net>
2014-03-15 11:39:01 -0400

    Updated.

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,55 +26,32 @@ module.exports = function(grunt) {
       src: 'src',
       dist: 'dist'
     },
-
-    watch: {
-      assemble: {
-        files: ['<%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
-        tasks: ['assemble']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-        files: [
-          '<%= config.dist %>/{,*/}*.html',
-          '<%= config.dist %>/assets/{,*/}*.css',
-          '<%= config.dist %>/assets/{,*/}*.js',
-          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
-    },
-
-    connect: {
+    assemble: {
       options: {
-        port: 9000,
-        livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        layoutdir: '<%= config.src %>/templates/layouts'
       },
-      livereload: {
+      posts: {
         options: {
-          open: true,
-          base: [
-            '<%= config.dist %>'
-          ]
+          flatten: true,
+          layout: 'entry.hbs'
+        },
+        files: {
+          '<%= config.dist %>/blog/': ['<%= config.src %>/content/posts/*.html']
         }
       }
-    },
-
-    assemble: {
+      /*
       pages: {
         options: {
           flatten: true,
           assets: '<%= config.dist %>/assets',
-          layout: '<%= config.src %>/templates/layouts/default.hbs',
+          layout: '<%= config.src %>/templates/layouts/main.hbs',
           data: '<%= config.src %>/data/*.{json,yml}',
           partials: '<%= config.src %>/templates/partials/*.hbs'
         },
         files: {
           '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
         }
-      }
+      }*/
     },
 
     // Before generating any new files,
