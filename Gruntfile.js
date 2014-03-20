e5d2d5d682f1789acf99fd74b79dae40ddbe6fc1
Kyle Rush <kyle@kylerush.net>
2014-03-20 10:59:50 -0400

    Updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,32 +26,92 @@ module.exports = function(grunt) {
       src: 'src',
       dist: 'dist'
     },
+    connect: {
+      options: {
+        port: 8000,
+        livereload: 35729,
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: ['<%= config.dist %>']
+        }
+      }
+    },
+    watch: {
+      assemble: {
+        files: [
+          '<%= config.src %>/content/posts/*.html',
+          '<%= config.src %>/templates/layouts/*.hbs'
+        ],
+        tasks: ['assemble']
+      },
+      css: {
+        files: ['<%= config.src %>/assets/css/main.scss'],
+        tasks: ['sass:compileCSS']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.dist %>/assets/css/*.css',
+          '<%= config.dist %>/content/blog/*.html'
+        ]
+      }
+    },
+    sass: {
+      compileCSS: {
+        files: {
+          '<%= config.dist %>/assets/css/main.css': '<%= config.src %>/assets/css/main.scss'
+        }
+      }
+    },
     assemble: {
       options: {
-        layoutdir: '<%= config.src %>/templates/layouts'
+        layoutdir: '<%= config.src %>/templates/layouts',
       },
       posts: {
         options: {
           flatten: true,
-          layout: 'entry.hbs'
+          layout: 'entry.hbs',
+          ext: '.html'
         },
         files: {
-          '<%= config.dist %>/blog/': ['<%= config.src %>/content/posts/*.html']
+          '<%= config.dist %>/blog/': ['<%= config.src %>/content/blog/*.html']
+        }
+      },
+      homepage: {
+        options: {
+          layout: 'main.hbs',
+          collections: [
+            {
+              name: 'posts',
+              infelction: 'post',
+              sortoder: 'asc',
+              orderby: 'date',
+              files: ['<%= config.src %>/content/blog/*.html']
+            }
+          ]
+        },
+        files: {
+          '<%= config.dist %>/index.html': ['<%= config.src %>/content/index.html']
         }
       }
-      /*
-      pages: {
-        options: {
-          flatten: true,
-          assets: '<%= config.dist %>/assets',
-          layout: '<%= config.src %>/templates/layouts/main.hbs',
-          data: '<%= config.src %>/data/*.{json,yml}',
-          partials: '<%= config.src %>/templates/partials/*.hbs'
-        },
+    },
+    copy: {
+      javascript: {
+        src: '<%= config.src %>/assets/js/polyfills/picturefill.js',
+        dest: '<%= config.dist %>/assets/js/main.js'
+      }
+    },
+    cssmin: {
+      minify: {
         files: {
-          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+          '<%= config.dist %>/assets/css/main.css': '<%= config.dist %>/assets/css/main.css'
         }
-      }*/
+      }
     },
 
     // Before generating any new files,
@@ -60,22 +120,32 @@ module.exports = function(grunt) {
 
   });
 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('server', [
-    'clean',
-    'assemble',
-    'connect:livereload',
-    'watch'
-  ]);
 
   grunt.registerTask('build', [
     'clean',
-    'assemble'
+    'sass',
+    'assemble',
+    'copy',
+    'cssmin'
   ]);
+
+  grunt.registerTask('server', [
+    'clean',
+    'sass',
+    'assemble',
+    'copy',
+    'cssmin',
+    'connect:livereload',
+    'watch'
+  ])
 
   grunt.registerTask('default', [
     'build'
